52eae608e9380823eab30358fe1ef9ba5a378f79
Punith P <punith.p@icloud.com>
2023-11-25 23:09:58 +0530

    prereq:solutions::p1-4.5

---
+++ Basics/Prerequisites/javaScript/functionAsyncSolutions.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// Question-1:  Write a function that takes an array and a callback function. Apply the callback function to each element of the array and return a new array with the modified elements.
+
+// Question-2:  Write a function that takes an array of numbers and returns a new array containing only the odd numbers.
+
+// Question-3:  Write a function that takes an array of numbers and returns a new array where each element is doubled.
+
+// Question-4:  Write a function that takes an array of strings and returns a new array where each string is transformed to uppercase.
+
+// Question-5:  Write a function that uses setTimeout to simulate an asynchronous operation. The function should print a message to the console after a specified delay.
+
+// Question-6:  WAP to perform read and write operation in a text file in JS.
---
+++ Basics/Prerequisites/javaScript/loopsSolutions.js
@@ -0,0 +1,45 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+// Question-1: WAP that logs numbers from 1 to 10 to the console.
+
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// }
+
+// Question-2: WAP that log all even numbers from 2 to 20.
+
+// for (let i = 1; i < 21; i++) {
+//   if (i % 2 == 0) console.log(i);
+// }
+
+// Question-3: WAP that counts backward from 10 to 1 and logs the values.
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+
+// Question-4: WAP to calculate the sum of numbers from 1 to 5.
+
+// let sum = 0;
+// for (let i = 0; i < 6; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+// Question-5: WAP that prints powers of 2 (2^n) up to 64.
+
+// let res = 1;
+// while (res <= 64) {
+//   res *= 2;
+//   console.log(res);
+// }
+
+// Question-6:  WAP to Develop a do-while loop for a simple number guessing game. Ask the user to guess a number between 1 and 10, and keep prompting until they guess correctly.
+
+// Question-7: WAP to display the multiplication table (1 to 10) in the console.
+
+// Question-8: WAP that use the break statement to exit the loop when the counter reaches 5. Also, use continue to skip printing the value 3 ( Note : Loop Starts from 0).
+
+// Question-9: WAP that logs numbers from 1 to 30. For multiples of 3, log "Fizz" instead of the number, and for multiples of 5, log "Buzz." For numbers which are multiples of both 3 and 5, log "FizzBuzz."
+
+// Question-10: WAP to identify and log prime numbers within a given range.
