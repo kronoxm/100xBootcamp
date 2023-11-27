936a6e32decec906817e0a0b7f10f71476a99b43
Punith P <punith.p@icloud.com>
2023-11-27 14:42:13 +0530

    prereq:solutions::p1-6

--- Basics/Prerequisites/javaScript/loopsPatternSolutions.js
+++ Basics/Prerequisites/javaScript/loopsPatternSolutions.js
@@ -1,4 +1,5 @@
 const prompt = require("prompt-sync")({ sigint: true });
+const n = 5;
 
 // Question-1:
 
@@ -10,6 +11,10 @@ const prompt = require("prompt-sync")({ sigint: true });
 // ****
 // *****
 
+for (let i = 1; i <= n; i++) {
+  console.log("*".repeat(i));
+}
+
 // Question-2:
 
 // WAP to print this
@@ -19,6 +24,15 @@ const prompt = require("prompt-sync")({ sigint: true });
 //  *****
 // *******
 
+const num = 7;
+for (let i = 1; i <= 4; i++) {
+  let str = "";
+  for (let j = 1; j <= num; j++) {
+    j >= 5 - i && j <= 3 + i ? (str += "*") : (str += " ");
+  }
+  console.log(str);
+}
+
 // Question-3:
 
 // WAP to print this
@@ -27,6 +41,18 @@ const prompt = require("prompt-sync")({ sigint: true });
 // *   *
 // *   *
 // *****
+
+for (let i = 1; i <= 4; i++) {
+  let str = "";
+  for (let j = 1; j <= 5; j++) {
+    if (i == 1 || i == 4 || j == 1 || j == 5) {
+      str += "*";
+    } else {
+      str += " ";
+    }
+  }
+  console.log(str);
+}
 
 // Question-4:
 
@@ -38,6 +64,12 @@ const prompt = require("prompt-sync")({ sigint: true });
 // 1234
 // 12345
 
+let temp = "1";
+for (let i = 1; i <= n; i++) {
+  console.log(temp);
+  temp += i + 1;
+}
+
 // Question-5
 
 // WAP to print this
@@ -47,3 +79,14 @@ const prompt = require("prompt-sync")({ sigint: true });
 //   CCCCC
 //  DDDDDDD
 // EEEEEEEEE
+
+const numb = 9;
+for (let i = 1; i <= 5; i++) {
+  let str = "";
+  for (let j = 1; j <= numb; j++) {
+    j >= 6 - i && j <= 4 + i
+      ? (str += String.fromCharCode(64 + i))
+      : (str += " ");
+  }
+  console.log(str);
+}
