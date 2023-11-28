b4049f3eb85645ffbc04dc20777d63da530729e6
Punith P <punith.p@icloud.com>
2023-11-28 17:06:19 +0530

    prereq:solutions::p1-8

--- Basics/Prerequisites/javaScript/arraysLCSolutions.js
+++ Basics/Prerequisites/javaScript/arraysLCSolutions.js
@@ -139,3 +139,19 @@ function insertionArr(arr1, arr2) {
 
 console.log(insertionArr([1, 2, 2, 1], [2, 2]));
 console.log(insertionArr([4, 9, 5], [9, 4, 9, 8, 4]));
+
+// Question-10
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+function buySellStocks(prices) {
+  const buyPrice = Math.min(...prices);
+  const newPrices = prices.slice(prices.indexOf(buyPrice));
+  const sellPrice = Math.max(...newPrices);
+  return newPrices.length > 1 ? prices.indexOf(sellPrice) + 1 : 0;
+}
+
+console.log(buySellStocks([7, 1, 5, 3, 6, 4]));
+console.log(buySellStocks([7, 6, 4, 3, 1]));
+console.log(buySellStocks([7, 6, 4, 3, 1, 2]));
