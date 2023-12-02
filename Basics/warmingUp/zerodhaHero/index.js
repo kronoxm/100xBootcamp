adfc966e69cdc0a0b25e0ededbce1cad40bfce9e
Punith P <punith.p@icloud.com>
2023-12-02 12:06:35 +0530

    prereq:warm-up:complete

--- Basics/warmingUp/zerodhaHero/index.js
+++ Basics/warmingUp/zerodhaHero/index.js
@@ -1,8 +1,23 @@
-const form = document.forms["returns-form"];
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-const getVals = (e) => {
-  e.preventDefault();
-  const capital = this.capital.value;
-  return capital;
+const getVals = () => {
+  const values = Array.from(
+    document.querySelectorAll("#returns-form input")
+  ).reduce(
+    (acc, inp) => ({ ...acc, [inp.id]: parseFloat(inp.value) || 0 }),
+    {}
+  );
+
+  if (Object.values(values).some((value) => value === 0)) {
+    alert("Please fill in all fields before calculating.");
+    return;
+  }
+
+  const finalAmount =
+    values.capital * (1 + values.interest / 100) ** values.years;
+  document.getElementById("final-amount").innerText = `$${numberWithCommas(
+    finalAmount.toFixed(2)
+  )}`;
 };
-form.addEventListener("submit", getVals);
