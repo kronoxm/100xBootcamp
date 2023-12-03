a792bffd9d0fa099793c732a2493efa425e01c43
Punith P <punith.p@icloud.com>
2023-12-04 00:23:55 +0530

    week_01

--- Week01/test.js
+++ Week01/test.js
@@ -1,10 +1,3 @@
-// let sum = 0;
-// for (let i = 0; i < 1000000000; i++) {
-//   setTimeout(function timeout() {
-//     i;
-//   }, 1000);
-// }
-
 let sum = 0;
 for (let i = 0; i < 1000000000000; i++) {
   sum = sum + i;
