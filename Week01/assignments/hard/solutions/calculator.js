7d878fdc69e6fbf91f638204a3d875f86173202e
Punith P <punith.p@icloud.com>
2023-12-05 00:30:27 +0530

    week_01:solutions[*PASSED]

--- Week01/assignments/hard/solutions/calculator.js
+++ Week01/assignments/hard/solutions/calculator.js
@@ -16,6 +16,10 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+  constructor(output) {
+    this.output = output;
+  }
+}
 
 module.exports = Calculator;
